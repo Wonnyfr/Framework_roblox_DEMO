5851dd9d57b3b3357a1e7bdf3ccbcc20da91fbc2
Wonnyfr <77871874+Wonnyfr@users.noreply.github.com>
2022-10-18 22:07:08 +1100

    Create Read.lua

---
+++ Server/Read.lua
@@ -0,0 +1,8 @@
+--// PLEASE UNDERSTAND THE CODE BEFORE PROGRESSING \\--
+
+--[1] Fires skill from the SkillReplicator 
+--[2] It reaches the server through the FireMove (ServerHandler: line 24)
+--[3] Go to Server side Combat where reaches the skill (Prints)
+--[4] Goes back to SkillReplicator, by OnClientEvent 
+--[5] Goes to VFX Module
+--[6] Goes to Client Combat (Prints)	
